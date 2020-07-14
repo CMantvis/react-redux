5c1d9f058d14a6a29c9fa8f56b623ad21317a980
CMantvis <civilka.mantas@gmail.com>
2020-07-14 14:52:34 +0300

    Connect navbar with redux, make redux actions and store"

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+// store - stores data, think of as a state
+// reducer - function that used to update store;
+// reducer has 
+// two arguments - state action
+// state - old state/state before update
+// action - what happened what update
+
+import reducer from "./reducer"
+import { createStore } from "redux";
+
+// dispatch method - send actions to the store
+// actions objects - must have type property - what kind of action
+// dont mutate the state - redux built on immutability (copy)
+
+// react-redux - PROVIDER -> wraps the app, connect -> used in components
+import {Provider} from "react-redux";
+
+
+//initial store
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 5
+};
+
+const store = createStore(reducer, initialStore);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </Provider>
+  );
+}
+
+export default App;
